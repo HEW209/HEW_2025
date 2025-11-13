36e1f97e2e3bfde1fc0c6276ad039ddc5b1a438d
mishina-rei <game.create123456@gmail.com>
2025-11-13 23:40:39 +0900

    collider.h作ったお(途中)

---
+++ Prototype/Collider.h
@@ -0,0 +1,15 @@
+/*****************************************************************//**
+ * @file   Collider.h
+ * @brief  当たり判定コンポーネント
+ *
+ * @author 三品怜
+ * @date   2025/11/13
+ *********************************************************************/
+#pragma once
+
+#include <DXGameFrame.h>
+
+class Collider :public Component
+{
+	Vector3 offset;
+};
