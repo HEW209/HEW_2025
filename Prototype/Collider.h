21d3008ec9f1245fb5bab4c8eb5e546874ebc164
mishina-rei <game.create123456@gmail.com>
2025-11-14 14:18:59 +0900

    collider.cpp作った

--- Prototype/Collider.h
+++ Prototype/Collider.h
@@ -8,8 +8,33 @@
 #pragma once
 
 #include <DXGameFrame.h>
+#include <vector>
+
+
 
 class Collider :public Component
 {
-	Vector3 offset;
+public:
+
+	struct Axis3
+	{
+		Vector3 x, y, z;
+	};
+
+	Collider();
+	~Collider() = default;
+
+	void Update()override;
+	void Awake()override;
+	void OnDestroy()override;
+
+	static void CheckCollision();
+
+private:
+
+	Transform m_transform;
+
+	static std::vector<Collider*> s_colliders;
+	static std::vector<Axis3> s_axis;
+
 };
