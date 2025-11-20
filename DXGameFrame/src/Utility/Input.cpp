d904d6bce58027e0f16570089e1ea98261aad5d6
rei1205 <rei1205.creator@gmail.com>
2025-11-20 17:58:47 +0900

    文字コード修正

--- DXGameFrame/src/Utility/Input.cpp
+++ DXGameFrame/src/Utility/Input.cpp
@@ -1,7 +1,7 @@
-// Input.cpp
+ï»¿// Input.cpp
 #include <Utility/Input.h>
 
-static const BYTE g_triggerDeadzone = 1000;		//ƒRƒ“ƒgƒ[ƒ‰[‚ÌƒgƒŠƒK[ƒfƒbƒhƒ][ƒ“
+static const BYTE g_triggerDeadzone = 1000;		//ã‚³ãƒ³ãƒˆãƒ­ãƒ¼ãƒ©ãƒ¼ã®ãƒˆãƒªã‚¬ãƒ¼ãƒ‡ãƒƒãƒ‰ã‚¾ãƒ¼ãƒ³
 
 uint8_t Input::s_oldKeyTable[MAX_KEY_TYPE];
 uint8_t Input::s_keyTable[MAX_KEY_TYPE];
@@ -10,11 +10,11 @@ XINPUT_STATE Input::s_padState;
 
 void Input::Init()
 {
-	// ƒL[“ü—Í‰Šú‰»
+	// ã‚­ãƒ¼å…¥åŠ›åˆæœŸåŒ–
 	memset(s_oldKeyTable, 0, sizeof(s_oldKeyTable));
 	bool dummy = GetKeyboardState(s_keyTable);
 
-	// ƒRƒ“ƒgƒ[ƒ‰[“ü—Í‰Šú‰»	
+	// ã‚³ãƒ³ãƒˆãƒ­ãƒ¼ãƒ©ãƒ¼å…¥åŠ›åˆæœŸåŒ–	
 	memset(&s_oldPadState, 0, sizeof(XINPUT_STATE));
 	memset(&s_padState, 0, sizeof(XINPUT_STATE));
 	XInputGetState(0, &s_padState);
@@ -22,11 +22,11 @@ void Input::Init()
 
 void Input::Update()
 {
-	// ƒL[“ü—ÍXV
+	// ã‚­ãƒ¼å…¥åŠ›æ›´æ–°
 	memcpy(s_oldKeyTable, s_keyTable, sizeof(s_oldKeyTable));
 	bool dummy = GetKeyboardState(s_keyTable);
 
-	// ƒRƒ“ƒgƒ[ƒ‰[“ü—ÍXV
+	// ã‚³ãƒ³ãƒˆãƒ­ãƒ¼ãƒ©ãƒ¼å…¥åŠ›æ›´æ–°
 	memcpy(&s_oldPadState, &s_padState, sizeof(XINPUT_STATE));
 	XInputGetState(0, &s_padState);
 }
@@ -50,7 +50,7 @@ bool Input::GetKeyUp(KeyCode keyType)
 
 bool Input::GetButtonHold(PadCode padCode)
 {
-	// ƒgƒŠƒK[ƒfƒbƒhƒ][ƒ“ˆ—
+	// ãƒˆãƒªã‚¬ãƒ¼ãƒ‡ãƒƒãƒ‰ã‚¾ãƒ¼ãƒ³å‡¦ç†
 	if (padCode == PadCode::LEFT_TRIGGER)
 	{
 		if (g_triggerDeadzone < s_padState.Gamepad.bLeftTrigger)
