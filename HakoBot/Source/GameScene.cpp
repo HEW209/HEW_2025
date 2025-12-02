c18cd98cd10af7f7597fa4b896ea47a3ec2ef32b
aoyagiseiun <seiunsukai108@gmail.com>
2025-12-02 15:56:58 +0900

    とちゅう

--- HakoBot/Source/GameScene.cpp
+++ HakoBot/Source/GameScene.cpp
@@ -1,20 +1,55 @@
 #include "GameScene.h"
+#include "Easing.h"
 
+SpriteRenderer* Renderer1 = nullptr;
 
 void GameScene::Init()
 {
+	
+
+	//カメラ
 	{
 		auto obj = CreateGameObject();
 		auto camera = obj->AddComponent<Camera>();
+	}
+
+	//メニューの十字
+	{
+		auto obj = CreateGameObject();
+		auto renderer = obj->AddComponent<SpriteRenderer>();
+		renderer->SetUI(true);
+		renderer->LoadTexture("Assets/Textures/menuplus.png");
+		renderer->GetTransform()->SetPosition(-4.2f, 1.7f, 0.0f);
+		renderer->SetSize(400.0f, 400.0f);
+	
+	}
+
+	//メニュー文字
+	{
+		auto obj = CreateGameObject();
+		auto renderer = obj->AddComponent<SpriteRenderer>();
+		renderer->SetUI(true);
+		renderer->LoadTexture("Assets/Textures/menu.png");
+		renderer->GetTransform()->SetPosition(-3.9f, 1.5f, 0.0f);
+		renderer->SetSize(450.0f, 450.0f);
+	
 	}
 	{
 		auto obj = CreateGameObject();
 		auto renderer = obj->AddComponent<SpriteRenderer>();
 		renderer->SetUI(true);
-		obj->GetTransform()->SetPosition(0.0f, 0.0f, 0.0f);
-		renderer->LoadTexture("Assets/Textures/menuplus.png");
-		renderer->SetSize(500.0f, 500.0f);
+		renderer->LoadTexture("Assets/Textures/sprite09.png");
+		renderer->GetTransform()->SetPosition(0.0f, 0.0f, 0.0f);
+		renderer->SetSize(450.0f, 450.0f);
+		Renderer1 = renderer;
 	}
+}
 
+void GameScene::Update()
+{
 	
+	Renderer1->GetTransform()->SetPosition(Easing::InSine(0.0f, 100.0f, 1.0f, 2.0f), 1.5f, 0.0f);
+	
+
+	Renderer1->SetSize(450.0f, 450.0f);
 }
--- HakoBot/Source/GameScene.h
+++ HakoBot/Source/GameScene.h
@@ -9,4 +9,5 @@ public:
 	~GameScene() = default;
 
 	void Init() override;
+	void Update();
 };
