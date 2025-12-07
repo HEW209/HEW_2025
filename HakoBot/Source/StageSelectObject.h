69ca037dce985b83edd14bc35301407f42f33419
23 Souemmon <souemmon@icloud.com>
2025-12-07 09:02:04 +0900

    ゲームシーンにコンポーネント追加 端末以降の為にコミット

--- HakoBot/Source/StageSelectObject.h
+++ HakoBot/Source/StageSelectObject.h
@@ -1,1 +1,24 @@
+/*****************************************************************//**
+ * @file   StageSelectObject.h
+ * @brief  ボタンが押されたらboolで返事する
+ *
+ * @author 石原颯也
+ * @date   2025/12/02	a
+ *********************************************************************/
 #pragma once
+
+#include <GameFrame/Component.h>
+#include <DirectX/DirectXInclude.h>
+#include <Utility/Vector2.h>
+
+ /**
+  * @brief ボタンについてのあれこれ
+  */
+
+class  : public Component
+{
+public:
+
+private:
+
+};
