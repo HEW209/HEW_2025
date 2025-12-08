73eeef2f5d610ba91ada3c666962f8867281e041
mishina-rei <game.create123456@gmail.com>
2025-12-09 01:23:12 +0900

    ClearProduce追加、仮の演出できた

--- HakoBot/Source/GameScene.cpp
+++ HakoBot/Source/GameScene.cpp
@@ -9,6 +9,8 @@
 #include "PlayerBlockHandler.h"
 #include "PlayerCamera.h"
 
+#include "ClearProduce.h"
+
 void GameScene::Init()
 {
 	KeyBind();
@@ -101,6 +103,10 @@ void GameScene::Init()
 		obj->GetTransform()->SetEulerAngle(50.0f, -30.0f, 0.0f);
 	}
 
+	{//ライト
+		auto obj = CreateGameObject();
+		obj->AddComponent<ClearProduce>();
+	}
 }
 
 void GameScene::KeyBind()
@@ -261,12 +267,12 @@ void GameScene::CreateGridField()
 	{
 		// スケーリング時の判定がおかしい
 		
-		//auto obj = CreateGameObject();
-		//obj->AddComponent<MeshRenderer>();
-		//obj->GetTransform()->SetPosition(0.0f, 0.0f, 0.0f);
-		//obj->GetTransform()->SetScale(size_x+1.0f, 0.2f, size_z);
-		//auto collider = obj->AddComponent<Collider>();
-		//collider->m_scale = Vector3(size_x + 2.0f, 0.01f, size_z + 2.0f);
+		/*auto obj = CreateGameObject();
+		obj->AddComponent<MeshRenderer>();
+		obj->GetTransform()->SetPosition(0.0f, 0.0f, 0.0f);
+		obj->GetTransform()->SetScale(size_x+1.0f, 0.2f, size_z);
+		auto collider = obj->AddComponent<Collider>();
+		collider->m_scale = Vector3(size_x + 2.0f, 0.02f, size_z + 2.0f);*/
 	}
 }
 
