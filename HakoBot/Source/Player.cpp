c796b3118bee20e2f5d5824e2e7e927f840edc8a
koolo2334 <4.8.koh.suke@gmail.com>
2025-12-08 11:01:58 +0900

    途中

--- HakoBot/Source/Player.cpp
+++ HakoBot/Source/Player.cpp
@@ -38,13 +38,4 @@ void Player::Awake()
 
 	// 支柱を登録
 	playerStretch->SetPillarObject(playerPillar);
-
-	//ブロックオブジェクトを生成
-	auto blockObj = SceneManager::GetActiveScene()->CreateGameObject();
-
-	//オフセットで位置を変える
-	blockObj->GetTransform()->SetPosition(0.0f, 2.0f, 0.0f);
-	auto blockComponent = blockObj->AddComponent<BlockObject>();
-	blockComponent->SetUseCollider(false);
-	blockHandler->SetBlockObject(blockComponent);
 }
--- HakoBot/Source/PlayerBlockHandler.cpp
+++ HakoBot/Source/PlayerBlockHandler.cpp
@@ -9,146 +9,51 @@ PlayerBlockHandler::PlayerBlockHandler()
 
 void PlayerBlockHandler::Awake()
 {
-	m_rotateRoot = SceneManager::GetActiveScene()->CreateGameObject();
+	{
+		auto obj = SceneManager::GetActiveScene()->CreateGameObject();
+		m_pBlockObject = obj->AddComponent<BlockObject>();
+		m_pBlockObject->SetUseCollider(false);
+	}
+
+	m_pBlockHolder = SceneManager::GetActiveScene()->CreateGameObject();
 }
 
 void PlayerBlockHandler::Update()
 {
 	auto blockTransform = m_pBlockObject->GetTransform();
 	auto playerTransform = GetTransform();
+	auto holderTransform = m_pBlockHolder->GetTransform();
+
+
+	Vector3 blockOffset{ 0.0f, 2.0f, 0.0f };
+	//地面に設置させるために計算
+	blockOffset += m_pBlockObject->GetGroundOffset();
+
+	holderTransform->SetPosition(playerTransform->GetPosition() + blockOffset);
 
 
 	if (InputSystem::GetButtonDown("RotateBlockRight"_hash)) {
-		blockTransform->Rotate(0.0f, -90.0f, 0.0f);
+		holderTransform->Rotate(0.0f, -90.0f, 0.0f);
 
 	}
 	if (InputSystem::GetButtonDown("RotateBlockLeft"_hash)) {
-		blockTransform->Rotate(0.0f, 90.0f, 0.0f);
+		holderTransform->Rotate(0.0f, 90.0f, 0.0f);
 
 	}
-
-	/*if (Input::GetKeyDown(KeyCode::KEY_1)) {
-		m_pBlockObject->GetTransform()->SetEulerAngle(0, 0, 0);
-
-		BlockSetData data;
-		data.blocks.resize(1);
-		data.blocks[0] = { 0, 0, 0 };
-		m_pBlockObject->SetBlockSet(data);
-	}
-	if (Input::GetKeyDown(KeyCode::KEY_2)) {
-		m_pBlockObject->GetTransform()->SetEulerAngle(0, 0, 0);
-
-		BlockSetData data;
-		data.blocks.resize(2);
-		data.blocks[0] = { 0, 0, 0 };
-		data.blocks[1] = { 1, 0, 0 };
-		m_pBlockObject->SetBlockSet(data);
-	}
-	if (Input::GetKeyDown(KeyCode::KEY_3)) {
-		m_pBlockObject->GetTransform()->SetEulerAngle(0, 0, 0);
-
-		BlockSetData data;
-		data.blocks.resize(3);
-		data.blocks[0] = { 0, 0, 0 };
-		data.blocks[1] = { 1, 0, 0 };
-		data.blocks[2] = { 0, 1, 0 };
-		m_pBlockObject->SetBlockSet(data);
-	}
-	if (Input::GetKeyDown(KeyCode::KEY_4)) {
-		m_pBlockObject->GetTransform()->SetEulerAngle(0, 0, 0);
-
-		BlockSetData data;
-		data.blocks.resize(4);
-		data.blocks[0] = { 0, 0, 0 };
-		data.blocks[1] = { 1, 0, 0 };
-		data.blocks[2] = { 0, 1, 0 };
-		data.blocks[3] = { 1, 1, 0 };
-		m_pBlockObject->SetBlockSet(data);
-	}
-	if (Input::GetKeyDown(KeyCode::KEY_5)) {
-		m_pBlockObject->GetTransform()->SetEulerAngle(0, 0, 0);
-
-		BlockSetData data;
-		data.blocks.resize(4);
-		data.blocks[0] = { 0, 0, 0 };
-		data.blocks[1] = { 1, 0, 0 };
-		data.blocks[2] = { 0, 1, 0 };
-		data.blocks[3] = { 0, 0, 1 };
-		m_pBlockObject->SetBlockSet(data);
-	}
-	if (Input::GetKeyDown(KeyCode::KEY_6)) {
-		m_pBlockObject->GetTransform()->SetEulerAngle(0, 0, 0);
-
-		BlockSetData data;
-		data.blocks.resize(5);
-		data.blocks[0] = { 0, 0, 0 };
-		data.blocks[1] = { 1, 0, 0 };
-		data.blocks[2] = { 0, 1, 0 };
-		data.blocks[3] = { 1, 1, 0 };
-		data.blocks[4] = { 0, 0, 1 };
-		m_pBlockObject->SetBlockSet(data);
-	}
-	if (Input::GetKeyDown(KeyCode::KEY_7)) {
-		m_pBlockObject->GetTransform()->SetEulerAngle(0, 0, 0);
-
-		BlockSetData data;
-		data.blocks.resize(6);
-		data.blocks[0] = { 0, 0, 0 };
-		data.blocks[1] = { 1, 0, 0 };
-		data.blocks[2] = { 0, 1, 0 };
-		data.blocks[3] = { 1, 1, 0 };
-		data.blocks[4] = { 0, 0, 1 };
-		data.blocks[5] = { 1, 0, 1 };
-		m_pBlockObject->SetBlockSet(data);
-	}
-	if (Input::GetKeyDown(KeyCode::KEY_8)) {
-		m_pBlockObject->GetTransform()->SetEulerAngle(0, 0, 0);
-
-		BlockSetData data;
-		data.blocks.resize(6);
-		data.blocks[0] = { 0, 0, 0 };
-		data.blocks[1] = { 1, 0, 0 };
-		data.blocks[2] = { 0, 1, 0 };
-		data.blocks[3] = { 1, 1, 0 };
-		data.blocks[4] = { 0, 0, 1 };
-		data.blocks[5] = { 1, 1, 1 };
-		m_pBlockObject->SetBlockSet(data);
-	}
-	if (Input::GetKeyDown(KeyCode::KEY_9)) {
-		m_pBlockObject->GetTransform()->SetEulerAngle(0, 0, 0);
-
-		BlockSetData data;
-		data.blocks.resize(7);
-		data.blocks[0] = { 0, 0, 0 };
-		data.blocks[1] = { 1, 0, 0 };
-		data.blocks[2] = { 0, 1, 0 };
-		data.blocks[3] = { 1, 1, 0 };
-		data.blocks[4] = { 0, 0, 1 };
-		data.blocks[5] = { 1, 0, 1 };
-		data.blocks[6] = { 0, 1, 1 };
-		m_pBlockObject->SetBlockSet(data);
-	}
-	if (Input::GetKeyDown(KeyCode::KEY_0)) {
-		m_pBlockObject->GetTransform()->SetEulerAngle(0, 0, 0);
-
-		BlockSetData data;
-		data.blocks.resize(8);
-		data.blocks[0] = { 0, 0, 0 };
-		data.blocks[1] = { 1, 0, 0 };
-		data.blocks[2] = { 0, 1, 0 };
-		data.blocks[3] = { 1, 1, 0 };
-		data.blocks[4] = { 0, 0, 1 };
-		data.blocks[5] = { 1, 0, 1 };
-		data.blocks[6] = { 0, 1, 1 };
-		data.blocks[7] = { 1, 1, 1 };
-		m_pBlockObject->SetBlockSet(data);
-	}*/
 
 
 	//プレイヤーからブロックを置く位置を決めるための相対オフセット（プレイヤー前方1.5m）
 	//Vector3 blockSize = playerTransform->GetQuaternion() * m_pBlockObject->GetSize();
 	//Vector3 placeCursorOffset{ 0.0f, 0.0f, -std::abs(blockSize.z) * 0.5f - 1.0f };
 	Vector3 placeCursorOffset{ 0.0f, 0.0f, -1.5f };
+
+	placeCursorOffset -= blockOffset;
+
+	placeCursorOffset += blockTransform->GetPosition();
+
+	playerTransform->GetQuaternion() * Vec3 { 0.0f, 0.0f, 1.0f };
+
+	placeCursorOffset += m_pBlockObject->GetSize();
 
 	//プレイヤーのクォータニオンを、相対オフセット方向に適用
 	//「プレイヤーの向いている方向に応じて、前方1.5mの位置」を求める。
@@ -263,19 +168,4 @@ void PlayerBlockHandler::Update()
 
 		}
 	}
-
-	
-	Vector3 blockOffset{ 0.0f, 2.0f, 0.0f };
-	//地面に設置させるために計算
-	blockOffset += m_pBlockObject->GetGroundOffset();
-	
-	blockTransform->SetPosition(playerTransform->GetPosition() + blockOffset);
-
-
-
-}
-
-void PlayerBlockHandler::SetBlockObject(BlockObject* pBlockObject)
-{
-	m_pBlockObject = pBlockObject;
 }
