5b16c1116e7ad5de091de5da5e8094bc788fce3f
rei1205 <rei1205.creator@gmail.com>
2026-01-26 16:19:28 +0900

    フェード調整+タイトルのコンベアにブロックが流れるように

--- HakoBot/Source/StageSelectScene.cpp
+++ HakoBot/Source/StageSelectScene.cpp
@@ -83,6 +83,10 @@ void StageSelectScene::Init()
 		renderer->SetSize(1280.0f);
 		renderer->SetBackGround(true);
 	}
+
+	// 使用していないリソース解放
+	TextureManager::Instance().CollectGarbage();
+	ModelManager::Instance().CollectGarbage();
 }
 
 
